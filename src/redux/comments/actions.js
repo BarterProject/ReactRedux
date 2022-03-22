8fcb9cd1caab187d8617c43698cb25a5c75fa6c9
melon0259 <melon0259@gmail.com>
2022-03-23 00:15:53 +0900

    리덕스 기초!!

--- src/redux/comments/actions.js
+++ src/redux/comments/actions.js
@@ -5,11 +5,30 @@ import {
   FETCH_COMMENTS_SUCCESS,
 } from "./types";
 
+export const fetchCommentsSuccess = (comments) => {
+  return {
+    type: FETCH_COMMENTS_SUCCESS,
+    payload: comments,
+  };
+};
+export const fetchCommentsFailure = (error) => {
+  return {
+    type: FETCH_COMMENTS_FAIL,
+    payload: error,
+  };
+};
+export const fetchCommentsRequest = () => {
+  return {
+    type: FETCH_COMMENTS_REQUEST,
+  };
+};
+
 export const fetchComments = () => {
   return (dispatch) => {
+    dispatch(fetchCommentsRequest);
     fetch("https://jsonplaceholder.typicode.com/comments")
       .then((response) => response.json())
-      .then((comments) => console.log(comments))
-      .catch((error) => console.log(error));
+      .then((comments) => dispatch(fetchCommentsSuccess(comments)))
+      .catch((error) => dispatch(fetchCommentsFailure(error)));
   };
 };
